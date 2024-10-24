b2b19353a93e6301b4999061d1953790ec193b98
Butyricoil <butyricoil@gmail.com>
2024-10-24 19:19:13 +0400

    eqalse and hash code

--- src/main/java/Colections/Main.java
+++ src/main/java/Colections/Main.java
@@ -4,12 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         Car car1 = new Car("Bmw1", 1);
-        //Car car2 = new Car("Bmw1", 1);
-        Car car2 = car1;
+        Car car2 = new Car("Bmw1", 1);
+//        Car car2 = car1;
         car2.setNumber(5);
-        System.out.println(car1.getNumber());
-        System.out.println(car1 == car2);
-        String brand = "BMW";
-        System.out.println(car1.getBrand() == brand);
+
+        System.out.println(car1.equals(car2));
+        System.out.println(car1.hashCode());
+        System.out.println(car2.hashCode());
     }
+
+
 }
